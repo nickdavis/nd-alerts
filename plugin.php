1030c7afb6224b556b2c6f1ab95bd5bb86658006
Nick Davis <nick@esanctuary.ch>
2018-07-31 08:32:07 +0200

    initial commit (10up scaffold defaults)

---
+++ plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: NdAlerts
+ * Plugin URI:
+ * Description:
+ * Version:     0.1.0
+ * Author:      10up
+ * Author URI:  https://10up.com
+ * Text Domain: nd-alerts
+ * Domain Path: /languages
+ *
+ * @package PluginScaffold
+ */
+
+// Useful global constants
+define( 'ND_ALERTS_VERSION', '0.1.0' );
+define( 'ND_ALERTS_URL', plugin_dir_url( __FILE__ ) );
+define( 'ND_ALERTS_PATH', dirname( __FILE__ ) . '/' );
+define( 'ND_ALERTS_INC', ND_ALERTS_PATH . 'includes/' );
+
+// Include files
+require_once ND_ALERTS_INC . 'functions/core.php';
+
+
+// Activation/Deactivation
+register_activation_hook( __FILE__, '\NdAlerts\Core\activate' );
+register_deactivation_hook( __FILE__, '\NdAlerts\Core\deactivate' );
+
+// Bootstrap
+NdAlerts\Core\setup();
