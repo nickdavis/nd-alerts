4b5ca04af6271db841f77dd74b37de17f561c738
Nick Davis <nick@esanctuary.ch>
2018-08-01 21:41:33 +0200

    clean up front end and JS

--- assets/js/frontend/frontend.js
+++ assets/js/frontend/frontend.js
@@ -1,73 +1,19 @@
 import Cookies from 'js-cookie';
 
-
-// import foo from './components/bar';
-
-// import * as Cookies from 'js-cookie';
-
-//import Cookies from '../../../node_modules/js-cookie/src/js.cookie.js';
-
-//console.log( 'alert.js outside jQuery' );
-
-document.body.classList.add( 'alert--on' );
-
-const closeButtons = document.getElementsByClassName( 'close_alert' );
-
-closeButtons[0].addEventListener( 'click', alertClose );
+const closeButtons = document.getElementsByClassName( 'nd-alerts__close' );
+const cookieValue = Cookies.get( 'nd-alerts' );
 
 function alertClose() {
-	Cookies.set( 'nd-alerts', 'hide', { expires: 0.006944444 } );
-	document.body.classList.remove( 'alert--on' );
+	Cookies.set( 'nd-alerts', 'hide', {expires: 0.006944444} );
+	document.body.classList.remove( 'nd-alerts--visible' );
 }
 
-// closeButtons[0].onClick = function() {
-// 	console.log( 'click' );
-// 	document.body.classList.remove( 'alert--on' );
-// };
+// Adds a click listener to the close button.
+closeButtons[ 0 ].addEventListener( 'click', alertClose );
 
-// closeButton.onClick = function() {
-// 	console.log( 'click 2nd function' );
-// 	document.body.classList.remove( 'alert--on' );
-// };
-
-
-
-
-
-//body.classList.add( 'alert--on' );
-
-//Cookies.set( 'nd-alerts', 'hide', { expires: 0.006944444 } );
-
-
-// jQuery( function ( $ ) {
-//
-// 	// COOKIES
-// 	// @link https://alexcican.com/post/set-cookies-javascript/
-//
-// 	$( 'body' ).addClass( 'alert--on' );
-//
-// 	// if the cookie is true, hide the alert box
-// 	// if ( $.cookie( 'hide-after-click' ) == 'yes' ) {
-// 	// 	$( 'body' ).removeClass( 'alert--on' );
-// 	// } else {
-// 	// 	$( 'body' ).addClass( 'alert--on' );
-// 	// }
-//
-// 	console.log( 'alert.js' );
-//
-// 	// when clicked on “X” icon do something
-// 	$( '.close_alert' ).click( function () {
-// 		Cookies.set( 'nd-alerts', 'hide', { expires: 0.006944444 } );
-//
-//
-// 		$( 'body' ).removeClass( 'alert--on' );
-// 		// add cookie setting that user has clicked
-// 		// $.cookie( 'hide-after-click', 'yes', {
-// 		// 	expires: 0.006944444, // expires = 10 mins of a day (10/1440)
-// 		// 	path: '/' // Set for all pages on the domain
-// 		// } );
-//
-// 		return false;
-// 	} );
-//
-// } );
+// If the cookie is set, hide the alert box.
+if ( cookieValue === 'hide' ) {
+	document.body.classList.remove( 'nd-alerts--visible' );
+} else {
+	document.body.classList.add( 'nd-alerts--visible' );
+}
--- templates/alert.php
+++ templates/alert.php
@@ -1,11 +1,11 @@
-<div class="nd-alerts__alert">
+<div class="nd-alerts">
     <div class="wrap">
         <p>
 			<?= $this->title ? '<strong>' . esc_html( $this->title ) . '</strong>' : ''; ?>
 			<?= $this->title && $this->text ? ' – ' : ''; ?>
 			<?= $this->text ? wp_kses_post( $this->text ) : ''; ?>
         </p>
-        <a href="#" class="close_alert"><img
+        <a href="#" class="nd-alerts__close"><img
                     src="<?php echo CHILD_THEME_URI; ?>/assets/images/icons/close_white.min.svg"
                     alt="Close Alert"></a>
     </div>
